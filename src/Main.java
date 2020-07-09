772ec11e7013e9bb2aa6e39d619fb107cc030963
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-09 15:15:56 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by KPS on 7/9/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int zz = 'Z' ;
+        System.out.println(zz);
+    }
+}
